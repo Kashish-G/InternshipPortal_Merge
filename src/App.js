6e4c23d20152f0c72406aef8d2193d1ce73f7be1
Kashish Gandhi <kashishgandhi6112003@gmail.com>
2023-04-29 00:40:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from './components/Signup';
+import ResponsiveDrawer from './components/ResponsiveDrawer';
+import Courses from './components/Courses';
+import LandingPage from './layout/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Login/> */}
+        <Router>
+        <Routes>
+          <Route path="/Signup" element={<Signup/>}>
+          </Route>
+          <Route path="/Login" element={<Login/>}>
+          </Route>
+          <Route path="/drawer" element={<ResponsiveDrawer/>}>
+          </Route>
+          <Route path="/" element={<Courses/>}>
+          </Route>
+          <Route path="/Landing" element={<LandingPage/>}>
+          </Route>
+
+
+        </Routes>
+      
+    </Router>
+    </div>
+  );
+}
+
+export default App;
